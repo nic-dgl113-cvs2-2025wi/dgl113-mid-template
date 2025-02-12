934c342a7d53fb554ada923bc46c1ecf7aeede51
Dibya Sarkar <Dibya.Sarkar@nic.bc.ca>
2025-02-12 09:48:38 -0800

    Add files via upload

---
+++ midterm25/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let items = [
+    "dog",
+    "cat",
+    "elephant",
+    "house",
+    "mouse",
+    "pineapple",
+    "grape",
+    "banana",
+    "chair",
+    "computer",
+    "acorn"
+];
+
+
+
+
+//^^^^^^^^^^^^^^^^
+/*
+write the function showItems as required
+
+*/
